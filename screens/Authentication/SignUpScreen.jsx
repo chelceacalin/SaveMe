8899f3e929a5d041a5be513d696ab985cba069c8
calin.chelcea <calin.chelcea@esolutions.ro>
2023-09-15 21:30:46 +0300

    finalized login

--- screens/Authentication/SignUpScreen.jsx
+++ screens/Authentication/SignUpScreen.jsx
@@ -3,65 +3,93 @@ import { StyleSheet, Text, View,Image,TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input, Button } from 'react-native-elements';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { ref,set,push } from '@firebase/database';
+import { db } from '../../config/firebase';
+
 
 const auth = getAuth()
+
 const SignUpScreen  = ({ navigation }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
   const [validationMessage, setValidationMessage] = useState('')
-
+  const [phone,setPhone]=useState("")
 
   let validateAndSet = (value,setValue) => {
    setValue(value)
 }
-function checkPassword(firstpassword,secondpassword) {
-  if(firstpassword !== secondpassword){
-    setValidationMessage('Password do not match') 
+
+
+async function createAccount() {
+  if (email === '' || password === '') {
+    setValidationMessage('Required fields are missing');
+    return;
   }
-  else setValidationMessage('')
-}
-  async function createAccount() {
-    email === '' || password === '' 
-    ? setValidationMessage('required filled missing')
-    : ''
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
+
+  try {
+    let userKey;
+    let success = false;
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((credential) => {
+        success = true;
+        userKey = credential.user.uid;
+      
+      
+        const newUserEntry = {
+          id: userKey,
+          email: email,
+          password: password,
+          phone: phone,
+        };
+
+
+
+        set(ref(db, `users/${userKey}`), newUserEntry);
+      });
+
+    if (success) {
+      setEmail('');
+      setPassword('');
+      setValidationMessage('');
       navigation.navigate('Sign In');
-    } catch (error) {
-      setValidationMessage(error.message);
+    } else {
+      setValidationMessage('User account creation failed');
     }
+  } catch (error) {
+    setValidationMessage(error.message);
   }
+}
+
+
+
   return (
     <View style={styles.container}>
       <View>
-      <Image  style={{width:150,height:150,alignSelf:'center'}}/>
         <Input
           placeholder='Email'
-          containerStyle={{marginTop: 10}}
+          containerStyle={{marginTop: 10,backgroundColor:'white',width:'auto'}}
           value={email}
           onChangeText={(text) => setEmail(text)}
           leftIcon={<Icon name='envelope' size={16}/>}
             />
         <Input
           placeholder='Password'
-          containerStyle={{marginTop:10}}
+          containerStyle={{marginTop: 10,backgroundColor:'white',width:'auto'}}
           value={password}
           onChangeText={(value) => validateAndSet(value, setPassword)}
           secureTextEntry
           leftIcon={<Icon name='key' size={16}/>}
-          
-
             />
-        <Input
-          placeholder='confirm password'
-          containerStyle={{marginTop:10}}
-          value={confirmPassword}
-          onChangeText={(value) => validateAndSet(value,setConfirmPassword)}
-          secureTextEntry
+
+       <Input
+          placeholder='Phone Number'
+          containerStyle={{marginTop: 10,backgroundColor:'white',width:'auto'}}
+          value={phone}
+          onChangeText={(value) => setPhone(value)}
           leftIcon={<Icon name='key' size={16}/>}
-          onBlur={()=>checkPassword(password,confirmPassword)}
-            />  
+            />
+
+
             {<Text style={styles.error}>{validationMessage}</Text>}
         <Button title="Sign up" buttonStyle={{marginTop:10}} onPress={createAccount} />
         <View>
