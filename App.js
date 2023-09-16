42e9768e9cf93f9072410e712add996dda18e445
calin.chelcea <calin.chelcea@esolutions.ro>
2023-09-16 10:33:52 +0300

    finalized navbar

--- App.js
+++ App.js
@@ -5,14 +5,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AuthStack from './screens/Routes/AuthStack';
 import HomeScreen from './screens/Home/Home';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native'; // Import StatusBar
 import CustomDrawerContent from './screens/drawer/CustomDrawer';
-import {
-  createDrawerNavigator
-} from "@react-navigation/drawer";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import StackNavigator from './screens/Routes/StackNavigator';
-
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
@@ -20,6 +17,7 @@ const Stack = createStackNavigator();
 function StackNavigatorWithDrawer() {
   return (
     <Drawer.Navigator
+      screenOptions={{ headerShown: false }}
       initialRouteName="Home"
       drawerContent={(props) => <CustomDrawerContent {...props} />}
     >
@@ -28,13 +26,14 @@ function StackNavigatorWithDrawer() {
   );
 }
 
-
-
 export default function App() {
   const { user } = useAuthentication();
 
   return (
     <NavigationContainer>
+      {/* Set StatusBar with the desired background color and bar style */}
+      <StatusBar backgroundColor="#000000" barStyle="light-content" />
+
       {user ? (
         <StackNavigatorWithDrawer />
       ) : (
