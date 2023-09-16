6073f47fa9c38e9a20220f6027a2d3e389c0fa89
radu.petrachioiu <radu.petrachioiu@esolutions.ro>
2023-09-16 05:19:00 +0300

    Silent loud emergency finished

--- screens/EmergencyConfig/SilentEmergencyConfig.jsx
+++ screens/EmergencyConfig/SilentEmergencyConfig.jsx
@@ -1,15 +1,182 @@
 import { StyleSheet, Text, View,Button } from 'react-native';
 import { useAuthentication } from '../../hooks/useAuthentication';
-import { getAuth, signOut } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ScrollView } from 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react';
+import { Modal } from 'react-native';
+import { TouchableHighlight } from 'react-native';
+import { TextInput } from 'react-native-web';
+import { db } from '../../config/firebase';
+import { set, ref, onValue} from "firebase/database";
+
+
 
 const auth = getAuth();
 
 export default function SilentEmergencyConfig() {
   const { user } = useAuthentication();
+
+  const [persons, setPersons] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [userModalVisible, setUserModalVisible] = useState(false);
+  const [activeModalPersonName, setActiveModalPersonName] = useState(null);
+  const [activeModalPersonPhoneNumber, setActiveModalPersonPhoneNumber] = useState(null);
+
+
+  const openUserModal = (person) => {
+    setActiveModalPersonName(person.name);
+    setActiveModalPersonPhoneNumber(person.phoneNumber);
+    setUserModalVisible(true);
+  };
+
+
+    useEffect(() => {
+        const getContacts = ref(db, `silentEmergencyContacts/${auth.currentUser.uid}/${phoneNumber}`);
+
+        onValue(getContacts, (snapshot) => {
+            const data = snapshot.val();
+            const tempPersons = [];
+            for (let id in data) {
+                
+                tempPersons.push(data[id]);
+            }
+            setPersons(tempPersons);
+            console.log("persons " + persons)
+            console.log(data);
+            });
+
+    }, []);
+
   return (
     <View style={styles.container}>
-      <Text>SILENT {user?.email}!</Text>
-      <Button title="Sign Out" style={styles.signOut} onPress={() => signOut(auth)} />
+      <Text>Silent Emergency Contact List</Text>
+
+      <TouchableOpacity 
+          style={styles.addContact}
+            onPress={() => setModalVisible(true)}
+        >
+          <Text style={styles.buttonText}> Add New Contact </Text>
+        </TouchableOpacity>
+        <ScrollView style={styles.scrollContainer}> 
+
+        <Modal
+          animationType="fade"
+          transparent={false}
+          visible={modalVisible}
+          onRequestClose={() => {
+            alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 22, alignItems: 'center'}}>
+            <View>
+              <Text>Add New Contact</Text>
+              <TextInput placeholder="Name" style={styles.modalItems} onChangeText={setName}/>
+              <TextInput placeholder="Phone Number" style={styles.modalItems} onChangeText={setPhoneNumber}/>
+
+              <TouchableOpacity
+                onPress={() => {
+                  setModalVisible(false);
+
+                }}>
+                <Text>Close</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={() => {
+                  setModalVisible(false);
+                  console.log(name);
+                  console.log(phoneNumber);
+                  const newUserEntry = {
+                  name : name,
+                  phoneNumber : phoneNumber,
+                  };
+
+                  set(ref(db, `silentEmergencyContacts/${auth.currentUser.uid}/${phoneNumber}`), newUserEntry);
+
+                }}>
+                <Text>Save</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+
+        {persons.map((person) => (
+            <View key={person?.phoneNumber}>
+            <TouchableOpacity
+                    onPress={() => openUserModal(person)}
+                >
+                <Text style={styles.item} key={person.phoneNumber}>{person.name}</Text>
+            </TouchableOpacity>
+
+            <Modal 
+                animationType="fade"
+                transparent={false}
+                visible={userModalVisible}
+                onRequestClose={() => {
+                    alert('Modal has been closed.');
+                }}>
+                <View style={{marginTop: 22, alignItems: 'center'}}>
+                    <View>
+                    <Text>User Details</Text>
+                    <TextInput value={activeModalPersonName} style={styles.modalItems} onChangeText={setActiveModalPersonName}/>
+                    <TextInput value={activeModalPersonPhoneNumber} style={styles.modalItems} onChangeText={setActiveModalPersonPhoneNumber}/>
+
+                    <View style={{width:"100%",display:'flex',flexDirection:'row',marginTop:20}}>
+                        <TouchableOpacity
+                            style = {{backgroundColor:"grey", width:'60px', display:'inline-block', margin:3}}
+                            onPress={() => {
+                            setUserModalVisible(false);
+                            setActiveModalPersonName(null);
+                            setActiveModalPersonPhoneNumber(null);
+                            }}>
+                            <Text 
+                            style = {{color:"white"}}
+                            >Cancel</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style = {{backgroundColor:"green", width:'60px', display:'inline-block',margin:3}}
+                            onPress={() => {
+                            setUserModalVisible(false);
+
+                            const newUserEntry = {
+                                name : activeModalPersonName,
+                                phoneNumber : activeModalPersonPhoneNumber,
+                            };
+                            set(ref(db, `silentEmergencyContacts/${auth.currentUser.uid}/${phoneNumber}`), newUserEntry);
+
+                            setActiveModalPersonName(null);
+                            setActiveModalPersonPhoneNumber(null);
+
+                            }}>
+                            <Text 
+                            style = {{color:"white"}}
+                            >Edit</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style = {{backgroundColor:"red", width:'60px', display:'inline-block',margin:3}}
+                            onPress={() => {
+                            set(ref(db, `silentEmergencyContacts/${auth.currentUser.uid}/${phoneNumber}`),null);
+                            setUserModalVisible(false);
+                            setActiveModalPersonName(null);
+                            setActiveModalPersonPhoneNumber(null);
+                            }}>
+                            <Text 
+                            style = {{color:"white"}}
+                            >Delete</Text>
+                        </TouchableOpacity>
+                    </View>
+                    </View>
+                </View>
+            </Modal>
+            
+            </View>
+
+        ))}
+        </ScrollView>
     </View>
   );
 }
@@ -20,9 +187,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 50,
   },
-  signOut:{
+  scrollContainer: {
+    backgroundColor: '#fff',
+    width: '100%',
+  },
+  addContact:{
     marginTop:10,
-    margin:'auto'
+    marginBottom:20,
+    padding:10,
+    margin:'auto',
+    backgroundColor:'black',
+  },
+  buttonText:{
+    color:'white'
+  },
+  item: {
+    padding: 10,
+    fontSize: 15,
+    marginTop: 5,
+  },
+  modalItems:{
+    margin:20,
   }
 });
