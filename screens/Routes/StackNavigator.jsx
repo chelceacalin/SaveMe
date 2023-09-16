188b2638809d2d76d544ce05981dbc3e1336aa37
sonia.banea <sonia.banea@esolutions.ro>
2023-09-16 04:24:07 +0300

    Send SMS with current location on button press

--- screens/Routes/StackNavigator.jsx
+++ screens/Routes/StackNavigator.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../Home/Home';
 import LoudEmergencyConfig from '../EmergencyConfig/LoudEmergencyConfig';
 import SilentEmergencyConfig from '../EmergencyConfig/SilentEmergencyConfig';
 
-import HomeScreen from '../Home/Home';
 const Stack = createStackNavigator();
-function StackNavigator() {
-    return (
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="home" component={HomeScreen} />
-          <Stack.Screen name="LoudEmergencyConfig" component={LoudEmergencyConfig} />
-          <Stack.Screen name="SilentEmergencyConfig" component={SilentEmergencyConfig} />
 
-        </Stack.Navigator>
-      );
+function StackNavigator() {
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
+			<Stack.Screen name="home" component={HomeScreen}/>
+			<Stack.Screen name="LoudEmergencyConfig" component={LoudEmergencyConfig}/>
+			<Stack.Screen name="SilentEmergencyConfig" component={SilentEmergencyConfig}/>
+		
+		</Stack.Navigator>
+	);
 }
 
 export default StackNavigator
